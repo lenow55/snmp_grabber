f7ddeadf24c97b136de60b38947394ffd556debb
Новиков Илья <nov.ilucha@yandex.ru>
2023-12-29 22:26:08 +0300

    добавил поле даты, и счётчик записей

--- snmp_daemon.py
+++ snmp_daemon.py
@@ -7,6 +7,7 @@ import os
 from typing import List
 from pyarrow import Schema, Table, csv, unify_schemas
 from pyarrow import parquet as pa_pq
+import pyarrow
 from time import sleep
 
 import logging
@@ -29,56 +30,68 @@ class snmpDaemon(Thread):
         self._if_table_oid: str = "1.3.6.1.2.1.2.2"
         self._if_x_table_oid: str = "1.3.6.1.2.1.31.1.1"
         self.writers = []
+        self.counter_flush:int = 0
         Thread.__init__(self)
 
     def initialize_first_request(self) -> Schema:
+        # переписать на простое получение таблиц через request
+        # и извлечения из них общей схемы
         counts_list = []
+        temp_values = []
         with concurent_f.ThreadPoolExecutor(max_workers=5) as executor:
             # Start the load operations and mark each future with its URL
-            future_to_url = {
+            future_to_oid = {
                 executor.submit(
                     self._get_table, self._if_table_oid): "if_table",
                 executor.submit(
-                    self._get_table, self._if_x_table_oid): "if_x_table"
+                    self._get_table, self._if_x_table_oid): "if_x_table",
+                executor.submit(
+                    self._get_table, self._if_x_table_oid): "dataTime"
             }
             logger.debug("start wait treads")
             for future in concurent_f.as_completed(
-                    future_to_url, timeout=15):
-                table_name = future_to_url[future]
+                    future_to_oid, timeout=15):
+                future_name = future_to_oid[future]
                 try:
                     logger.debug('%r check this table' %
-                                 table_name)
+                                 future_name)
                     data = future.result()
                     logger.debug(
                         "Get resulted bytes success")
                 except Exception as exc:
                     logger.error(
                         '%r generated an exception: %s' %
-                        (table_name, exc))
+                        (future_name, exc))
                     raise Exception(
                         "Can't initialize daemon")
                 else:
                     temp_table: Table = csv.read_csv(
                         data, read_options=csv.ReadOptions(
                             skip_rows=2))
-                    if table_name == "if_table":
+                    if future_name == "if_table":
                         self.if_table_schema = temp_table.schema
                         counts_list.append(
                             temp_table.num_rows)
-                    elif table_name == "if_x_table":
+                    elif future_name == "if_x_table":
                         self.if_x_table_schema = temp_table.schema
                         counts_list.append(
                             temp_table.num_rows)
+                    else:
+                        temp_values.append(future_name)
 
         self.schema = unify_schemas([
             self.if_table_schema,
             self.if_x_table_schema
         ])
+        for value_name in temp_values:
+            self.schema.append(
+                pyarrow.field(
+                    value_name, pyarrow.string()))
         if not all(x == counts_list[0]
                    for x in counts_list):
             raise Exception(
-                f"Не одинаковое количество интерфейсов в таблицах\n\
-                        {counts_list}")
+                "Не одинаковое количество интерфейсов в таблицах\n" +
+                f"{counts_list}")
         self._count_interfaces = counts_list[0]
         self._initialized_schema = True
         return self.schema
@@ -112,6 +125,36 @@ class snmpDaemon(Thread):
             "-v2c", "-c", "sec", "-Cf", ",",
             "-Cr", str(self.batch_download_size),
             self.snmp_agent_ip, table_oid
+        ]
+        # Выполняем команду в системном шелле
+        try:
+            process = subprocess.run(
+                commands,
+                check=True,
+                timeout=self._timeout_snmp_subprocess,
+                capture_output=True)
+            result.write(process.stdout)
+        except subprocess.CalledProcessError as e:
+            raise Exception(
+                f"Произошла ошибка при получении данных по snmp {e}")
+        except subprocess.TimeoutExpired as e:
+            raise Exception(
+                f"Процесс опроса snmp завершён по таймауту {e}")
+        result.seek(0)
+        return result
+
+    def _get_value(self, oid: str) -> BytesIO:
+        """
+        Вход:
+            oid: oid значения в snmp, не понятно что будет,
+            если он не будет найден
+        """
+        result: BytesIO = BytesIO()
+        commands: List[str] = [
+            "snmpget",
+            "-t", str(self._timeout_snmp),
+            "-v2c", "-c", "sec", "-Oq",
+            self.snmp_agent_ip, oid
         ]
         # Выполняем команду в системном шелле
         try:
@@ -171,42 +214,58 @@ class snmpDaemon(Thread):
 
     def request(self) -> Table:
         temp_tables = {}
+        temp_values = {}
         with concurent_f.ThreadPoolExecutor(max_workers=5) as executor:
             # Start the load operations and mark each future with its URL
-            future_to_url = {
+            future_to_oid = {
                 executor.submit(
                     self._get_table, self._if_table_oid): "if_table",
                 executor.submit(
-                    self._get_table, self._if_x_table_oid): "if_x_table"
+                    self._get_table, self._if_x_table_oid): "if_x_table",
+                executor.submit(
+                    self._get_table, self._if_x_table_oid): "dataTime"
             }
             logger.debug("start wait requests treads")
             for future in concurent_f.as_completed(
-                    future_to_url, timeout=15):
-                table_name = future_to_url[future]
+                    future_to_oid, timeout=15):
+                future_name = future_to_oid[future]
                 try:
                     logger.debug('%r check this table' %
-                                 table_name)
+                                 future_name)
                     data = future.result()
                     logger.debug(
                         "Get resulted bytes success")
                 except Exception as exc:
                     logger.error(
                         '%r generated an exception: %s' %
-                        (table_name, exc))
+                        (future_name, exc))
                     raise Exception(
-                        f"Error with request table {table_name}")
+                        f"Error with request table {future_name}")
                 else:
-                    temp_tables.update(
-                        {table_name: csv.read_csv(
-                            data,
-                            read_options=csv.ReadOptions(
-                                skip_rows=2))
-                         })
+                    if future_name.split('_')[-1] == "table":
+                        temp_tables.update(
+                            {future_name: csv.read_csv(
+                                data,
+                                read_options=csv.ReadOptions(
+                                    skip_rows=2))
+                             })
+                    else:
+                        value: str = data.getvalue(
+                        ).decode().split(' ')[-1]
+                        temp_values.update(
+                            {future_name: value})
+
         out_table = temp_tables["if_table"]
         for name, col in zip(
                 temp_tables["if_x_table"].column_names,
                 temp_tables["if_x_table"].columns):
             out_table = out_table.append_column(name, col)
+        for name, value in temp_values.items():
+            arr = pyarrow.array(
+                # дублирую значения, чтобы в каждой строчке оказались
+                [value for i in self._count_interfaces],
+                type=pyarrow.string())
+            out_table = out_table.append_column(name, arr)
 
         return out_table
 
@@ -217,4 +276,5 @@ class snmpDaemon(Thread):
                 "Неправильное количество батчей")
         for index, batch in enumerate(batches):
             self.writers[index].write_batch(batch)
-        logger.info("data flushed")
+        self.counter_flush += 1
+        logger.info(f"data flushed #{self.counter_flush}")
